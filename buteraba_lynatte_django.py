c99230fc767bcdec004a537e65be3c5683f146b1
Lynatte Buteraba <107687661+lynnbute@users.noreply.github.com>
2023-07-31 20:36:07 +0300

    Add files via upload

---
+++ buteraba_lynatte_django.py
@@ -0,0 +1,16 @@
+#Web Development Framework
+"""
+Web development with Beautiful Soup
+Python MySql and Mongo DB
+Getting started with Django
+-Create Virtual environment
+-Install Django
+-Create project
+-Create App
+-Django views
+-URLs
+-Template
+-Model
+-Insert, update, delete data
+"""
+#Introduction to Django
